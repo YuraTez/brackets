1ac808fc72aaa85ef6a7f7a2a72e45d4ab25587d
YuraTez <52445676+YuraTez@users.noreply.github.com>
2020-03-22 14:18:46 +0300

    feat:decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+     let  stack = [];
+for(let i=0; i<bracketsConfig.length; i++){
+    stack.push(bracketsConfig[i].join(''));
 }
+for(let j=0; j<stack.length;){
+    if (str.includes(stack[j])){
+        str=str.replace(stack[j],'');
+        j=0;
+    }
+    else j++;
+}
+    let result;
+    str.length > 0 ? result = false: result = true;
+    return result;
+};
+
